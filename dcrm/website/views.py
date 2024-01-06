01749168e8b6d790a06ae5e18098d1b371e600ba
Saba-Baroughi <saba.baroughi1111@gmail.com>
2024-01-06 17:56:03 +0000

    table

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 
 
 
 def home(request):
+	records=Record.objects.all()
 	
 	# Check to see if logging in
 	if request.method == 'POST':
@@ -23,7 +25,7 @@ def home(request):
 			messages.success(request, "There Was An Error Logging In, Please Try Again...")
 			return redirect('home')
 	else:
-		return render(request, 'home.html', {})
+		return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
