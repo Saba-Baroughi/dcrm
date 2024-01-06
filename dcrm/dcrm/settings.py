56925b4b2f3456faf1679c4d120117b55441e94b
Saba-Baroughi <saba.baroughi1111@gmail.com>
2024-01-06 21:53:22 +0000

    Heroku

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-mx3p*o(7k1!yp_lyyai4eel^fey%_*st(ss0=v(++xb_@t!2@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
